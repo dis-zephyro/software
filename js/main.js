689500c62cb0333c98846496b7b8348c7dca453b
dis-zephyro <zephyro@yandex.ru>
2015-11-21 01:44:25 +0300

    vq

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Toddle items
+
+$('.btn-toddle').click(function(e){
+    e.preventDefault();
+    var menu = $($(this).attr("data-target"));
+    console.log(menu);
+    menu.slideToggle('fast');
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 768 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
+
+// Slider
+
+$('.slider-body').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
